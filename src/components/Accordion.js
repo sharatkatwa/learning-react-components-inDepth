6291d4ee3663c1df792e5f80e255c07be9c40a5a
Sharat Katwa <sjkatwa2000@gmail.com>
2023-04-15 19:14:47 +0530

    learned how to make accordion in react and created it

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,12 +1,44 @@
-function Accordion({ items }) {
-  const renderedItems = items.map((item) => (
-    <div key={item.id}>
-      <div>{item.label}</div>
-      <div>{item.content}</div>
-    </div>
-  ))
+import { useState } from 'react'
+import { GoChevronLeft, GoChevronDown } from 'react-icons/go'
 
-  return <div>{renderedItems}</div>
+function Accordion({ items }) {
+  const [expandedIndex, setExpandedIndex] = useState(-1)
+
+  const handleClick = (nextIndex) => {
+    setExpandedIndex((currentExpandedIndex) => {
+      if (currentExpandedIndex === nextIndex) {
+        return -1
+      } else {
+        return nextIndex
+      }
+    })
+  }
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = expandedIndex === index
+
+    const icon = (
+      <span className='text-2xl'>
+        {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+      </span>
+    )
+    return (
+      <div key={item.id}>
+        <div
+          className='flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer'
+          onClick={() => {
+            handleClick(index)
+          }}
+        >
+          {item.label}
+          {icon}
+        </div>
+        {isExpanded && <div className='border-b p-5'>{item.content}</div>}
+      </div>
+    )
+  })
+
+  return <div className='border-x border-t rounded'>{renderedItems}</div>
 }
 
 export default Accordion
--- src/pages/AccordionPage.js
+++ src/pages/AccordionPage.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import Accordion from '../components/Accordion'
 
 function AccordionPage() {
-  return <div>AccordionPage</div>
+  const items = [
+    {
+      id: '1',
+      label: 'Can we use react in our projects?',
+      content:
+        'is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to ',
+    },
+    {
+      id: '2',
+      label: 'Can we use JavaScript in our projects?',
+      content:
+        'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,',
+    },
+    {
+      id: '3',
+      label: 'Can we use HTML and CSS in our projects?',
+      content:
+        'Of course you can use in your projects its your project who care about what you use, You can use anything you want',
+    },
+  ]
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  )
 }
 
 export default AccordionPage
