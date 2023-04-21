ab1fe8a47a438df579b9fee7d421e4895cf924de
Sharat Katwa <sjkatwa2000@gmail.com>
2023-04-21 17:00:32 +0530

    refactor count page using reducer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -3,10 +3,36 @@ import Button from '../components/Button'
 import Panel from '../components/Panel'
 // import useCounter from '../hooks/use-counter'
 
+const INCREMENT_COUNT = 'increment'
+const DECREMENT_COUNT = 'decrement'
+const SET_VALUE_TO_ADD = 'set-value-to-add'
+const ADD_VALUE_ON_SUBMIT = 'add-value-on-submit'
+
 const reducer = (state, action) => {
-  return {
-    ...state,
-    count: state.count + 1,
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count + 1,
+      }
+    case DECREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count - 1,
+      }
+    case SET_VALUE_TO_ADD:
+      return {
+        ...state,
+        valueToAdd: action.payload,
+      }
+    case ADD_VALUE_ON_SUBMIT:
+      return {
+        ...state,
+        count: state.count + state.valueToAdd,
+        valueToAdd: 0,
+      }
+    default:
+      return state
   }
 }
 
@@ -18,18 +44,33 @@ function CounterPage({ initialCount }) {
 
   const increment = () => {
     // setCount(count + 1)
-    dispatch()
+    dispatch({
+      type: INCREMENT_COUNT,
+    })
   }
   const decrement = () => {
     // setCount(count - 1)
+    dispatch({
+      type: DECREMENT_COUNT,
+    })
   }
   const handleChange = (e) => {
     const value = parseInt(e.target.value) || 0
+
+    dispatch({
+      type: SET_VALUE_TO_ADD,
+      payload: value,
+    })
 
     // setValueToAdd(value)
   }
   const handleSubmit = (e) => {
     e.preventDefault()
+
+    dispatch({
+      type: ADD_VALUE_ON_SUBMIT,
+    })
+
     // setCount(count + valueToAdd)
     // setValueToAdd(0)
   }
@@ -48,8 +89,8 @@ function CounterPage({ initialCount }) {
       <form onSubmit={handleSubmit}>
         <label>Add a Lot</label>
         <input
-          className='p-1 m-3 bg-gray-50 border'
           type='number'
+          className='p-1 m-3 bg-gray-50 border'
           value={state.valueToAdd || ''}
           onChange={handleChange}
         />
